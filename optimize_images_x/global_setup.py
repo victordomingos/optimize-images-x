c42c15fbee20fe82e05f2f00355ab8cea10cb9d2
victordomingos <editor.arcosonline@gmail.com>
2021-04-05 20:18:24 +0100

    constants global setup (WIP)

--- optimize_images_x/global_setup.py
+++ optimize_images_x/global_setup.py
@@ -1,4 +1,14 @@
 import os
 
 # todo: account for windows paths...
-DB_PATH = os.path.expanduser("~") + "/optimize_images_x_settings.sqlite"
+DB_PATH = os.path.expanduser('~') + '/optimize_images_x_settings.sqlite'
+
+APP_PATH = 'xxx'
+APP_NAME = 'Optimize Image X'
+APP_LICENSE = 'copyright text'
+
+CREDITS = """
+    credits text
+    sample text 
+    more sample text
+    """

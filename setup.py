85108760bc67ed29552baf742e8f8b8225efb12c
victordomingos <editor.arcosonline@gmail.com>
2021-04-21 23:33:59 +0100

    requirepentes/setup.py

--- setup.py
+++ setup.py
@@ -67,8 +67,9 @@ setup(name='optimize-images-x',
 
       install_requires=[
           'optimize-images>=1.4.0',
-          'pillow>=8.0.1',
-          'piexif>=1.1.3'
+          'pillow>=8.2.0',
+          'piexif>=1.1.3',
+          'watchdog>=2.0.2'
       ],
 
       entry_points={
